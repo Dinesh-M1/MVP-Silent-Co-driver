d6c26e53d95641b0c34a0821b3eed9e16e147878
Dinesh <developer@mvp-silent-codriver.local>
2026-08-11 00:08:45 +0530

    Auto-save commit: 2026-08-11 00:08:45

--- frontend/src/components/page.tsx
+++ frontend/src/components/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b9dfd0f14e021ea0177f2e3feebce249166db18fa332881837da2a2b95dbe61
-size 10413
+oid sha256:a451d3a8c1098469799682ecfb061746a884e0f46c9b4d45cc91d1554ef78e0f
+size 7810
