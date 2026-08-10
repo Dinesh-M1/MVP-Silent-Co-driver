1fef3450808f79c8c6afaf802515af68cd3c82e8
Dinesh <developer@mvp-silent-codriver.local>
2026-08-11 00:50:23 +0530

    Auto-save commit: 2026-08-11 00:50:22

--- frontend/frontend/next.config.mjs
+++ frontend/frontend/next.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dcfc25b1ac1a5e0ae5a39763bb28681353201171397d5e7ef0ab36bcba2ff4d
-size 210
+oid sha256:d3acd5e4e09408959ebef17df01a7d40d578d3a59709ef2e8a9429932a256ef7
+size 213
