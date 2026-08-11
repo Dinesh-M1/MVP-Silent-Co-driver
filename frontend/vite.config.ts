8429e83a8ea867636a7ed0f3e4eb0922313b3a26
Dinesh <developer@mvp-silent-codriver.local>
2026-08-11 10:50:55 +0530

    Auto-save commit: 2026-08-11 10:50:55

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43960751dda5b5b411ae63b7f3ee73849149cda1ffb4fd380b8244631a6b2c78
-size 190
+oid sha256:961d405ff96daa09f3d5b51567cfe7ee619251b56c39f49b5eaa182b2741ca30
+size 326
