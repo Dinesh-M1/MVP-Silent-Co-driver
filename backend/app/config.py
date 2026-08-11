9c330bedcf9ba8e1d26ba399ba1f56335076533f
Dinesh <developer@mvp-silent-codriver.local>
2026-08-11 11:27:14 +0530

    Auto-save commit: 2026-08-11 11:27:14

---
+++ backend/app/config.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fa87a6168dcd85fba1e5fb745fa55fe1e0cd73e680dd09045296511097be8d4d
+size 64
---
+++ backend/app/schemas/telemetry.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:446b27a20b29f15db5f04165576663dd38305c52e0adb280ee2c8fe99d3e9dd5
+size 176
