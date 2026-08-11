be060c6b99ea2a4c007089275fdc44859eee905e
Dinesh <developer@mvp-silent-codriver.local>
2026-08-11 11:26:59 +0530

    Auto-save commit: 2026-08-11 11:26:58

---
+++ backend/app/core/llm_client.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7dff58374ead4f702c990105637f1b056a3ed977d53d8f74ddb80442da9aa04d
+size 99
---
+++ backend/app/core/vision_processor.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:378f3b0cc83d5a699a46fa916ad57493d2a7a4b658fbe8726d231651fd9a5e17
+size 109
