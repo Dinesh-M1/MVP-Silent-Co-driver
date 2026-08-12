2fad62d3794428fcad979964a3bbbc1d1fcf4faa
Dinesh-M1 <mummareddydinesh11@gmail.com>
2026-08-12 17:58:43 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8e163c3d9591b3bc89d16649081507b4ca4b7d32aed6e2b4b3c5d30d27f7391e
-size 117
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default nextConfig;
