b9aea148a76f91581aa3ae8f558fce9a5d8ee6b8
Dinesh-M1 <mummareddydinesh11@gmail.com>
2026-08-12 17:38:50 +0530

    Update setup_env.ps1

--- backend/setup_env.ps1
+++ backend/setup_env.ps1
@@ -1,3 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:845a74fd3ba542321eec20897951d2f74192e1ebbe53f281cfcc22b96bcb7c98
-size 80
+python -m venv venv
+.\venv\Scripts\Activate.ps1
+pip install -r requirements.txt
